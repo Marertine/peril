fd07f052524d608bded1a289e2a2ac33c4bbde5f
Marertine <rhodrirhamantus@gmail.com>
2026-01-15 14:20:23 +1100

    Ch3-1: Publishers and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,20 +7,32 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 	fmt.Println("Starting Peril server...")
 
 	// Create a new connection to RabbitMQ
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
-		log.Println("Unable to connect to RabbitMQ at localhost:5672")
+		log.Fatalf("Unable to connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
 
-	fmt.Println("Successfully connected to RabbitMQ instance")
+	fmt.Println("Peril game server connected to RabbitMQ")
+
+	// Create a new channel
+	newChannel, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Unable to create new channel")
+	}
+
+	// Publish a message to the exchange
+	err = pubsub.PublishJSON(newChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
