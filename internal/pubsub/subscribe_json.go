6d6c3733daf63995377b73c8b8d34c5eef978218
Marertine <rhodrirhamantus@gmail.com>
2026-01-16 15:10:50 +1100

    Ch5-2: Acknowledge and NotAcknowledge

--- internal/pubsub/subscribe_json.go
+++ internal/pubsub/subscribe_json.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	//"context"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	//"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
@@ -11,7 +12,15 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType, handler func(T)) error {
+type Acktype int
+
+const (
+	Ack Acktype = iota
+	NackDiscard
+	NackRequeue
+)
+
+func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType, handler func(T) Acktype) error {
 	// Ensure that the queue exists and is bound to the exchange
 	chanDelivery, queueDelivery, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
@@ -39,6 +48,19 @@ func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string
 				log.Printf("Unable to acknowledge messageID: %s: %v", msg.MessageId, err)
 				continue
 			}
+
+			switch handler(v) {
+			case Ack:
+				msg.Ack(false)
+				fmt.Println("Ack")
+			case NackDiscard:
+				msg.Nack(false, false)
+				fmt.Println("NackDiscard")
+			case NackRequeue:
+				msg.Nack(false, true)
+				fmt.Println("NackRequeue")
+			}
+
 		}
 	}()
 
